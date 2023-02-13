4352ac53fcd61e7a1a31ab1e89cd659cd1f6b244
Shreyas Jagtap <84124624+shreyas5522@users.noreply.github.com>
2023-02-13 16:04:28 +0530

    Update management.py

--- management.py
+++ management.py
@@ -4,8 +4,8 @@ class Library:
 
     def display_books(self):
         print("List of books:")
-        for book in self.books:
-            print(f"Title: {book['title']} | Author: {book['author']} | ISBN: {book['ISBN']}")
+        for index, book in enumerate(self.books):
+            print(f"{index + 1}. Title: {book['title']} | Author: {book['author']} | ISBN: {book['ISBN']}")
 
     def add_book(self, title, author, ISBN):
         book = {
@@ -30,12 +30,41 @@ class Library:
         else:
             print("No books found.")
 
+def menu():
+    print("Library Management System")
+    print("1. Display books")
+    print("2. Add book")
+    print("3. Search book")
+    print("4. Quit")
+
 if __name__ == "__main__":
     library = Library()
-    library.add_book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", "0-345-39180-2")
-    library.add_book("To Kill a Mockingbird", "Harper Lee", "0-449-94970-8")
-    library.display_books()
-    books = library.search_book(author="Douglas Adams")
-    print("Books by Douglas Adams:")
-    for book in books:
-        print(f"Title: {book['title']} | ISBN: {book['ISBN']}")
+    while True:
+        menu()
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            library.display_books()
+        elif choice == 2:
+            title = input("Enter book title: ")
+            author = input("Enter book author: ")
+            ISBN = input("Enter book ISBN: ")
+            library.add_book(title, author, ISBN)
+        elif choice == 3:
+            search_type = int(input("Search by:\n1. Title\n2. Author\n3. ISBN\nEnter your choice: "))
+            if search_type == 1:
+                title = input("Enter book title: ")
+                books = library.search_book(title=title)
+            elif search_type == 2:
+                author = input("Enter book author: ")
+                books = library.search_book(author=author)
+            elif search_type == 3:
+                ISBN = input("Enter book ISBN: ")
+                books = library.search_book(ISBN=ISBN)
+            if books:
+                for book in books:
+                    print(f"Title: {book['title']} | Author: {book['author']} | ISBN: {book['ISBN']}")
+        elif choice == 4:
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Try again.")
